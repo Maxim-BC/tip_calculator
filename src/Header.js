9e7d64978f0630cf42d6bd2a0dc52c65e31c9a12
Maxim-BC <76106605+Maxim-BC@users.noreply.github.com>
2021-07-22 13:14:16 +0300

    add project tip_calculator

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="main-content">
+        <h1>Калькулятор чаевых</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
